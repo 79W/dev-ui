57910527cc0f54b6f0e069968630f54093d33802
乔越 <qiaoyue@weiche>
2022-06-22 17:21:34 +0800

    更新doc

--- src/components/flex/demos/demo.tsx
+++ src/components/flex/demos/demo.tsx
@@ -1,36 +1,41 @@
 import React from "react";
-import { Flex } from 'kf-ui'
+import { Flex } from '../../../index'
 import './index.less'
 
 function Demo (){
     return <div className="demo-flex">
-        <p className="demo-flex-title" ></p>
+        <p className="demo-flex-title" >基础用法</p>
         <div className="demo-flex-content">
-            <Flex gutter={10}>
-                <Flex.Item span={8}>
-                    <span style={{
-                        fontSize:14,
-                        width:'100%',
-                        display:'inline-block',
-                        background:'red'
-                    }}>你好1</span> 
-                </Flex.Item>
-                <Flex.Item span={8}>
-                    <span style={{
-                        fontSize:14,
-                        width:'100%',
-                        display:'inline-block',
-                        background:'red'
-                    }}>你好2</span> 
-                </Flex.Item>
-                <Flex.Item span={8}>
-                    <span style={{
-                        fontSize:14,
-                        width:'100%',
-                        display:'inline-block',
-                        background:'red'
-                    }}>你好3</span> 
-                </Flex.Item>
+            <Flex justify="center" align="center">
+                <Flex.Item span={12}>span: 12</Flex.Item>
+                <Flex.Item span={12}>span: 12</Flex.Item>
+            </Flex>
+            
+            <Flex>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+            </Flex>
+        </div>
+        <p className="demo-flex-title" >区域间隔</p>
+        <div className="demo-flex-content">
+            <Flex gutter={16}>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+                <Flex.Item span={8}>span: 8</Flex.Item>
+            </Flex>
+        </div>
+        <p className="demo-flex-title" >方向</p>
+        <div className="demo-flex-content">
+            <Flex direction="row">
+                <Flex.Item span={8}>span: 8-1</Flex.Item>
+                <Flex.Item span={8}>span: 8-2</Flex.Item>
+                <Flex.Item span={8}>span: 8-3</Flex.Item>
+            </Flex>
+            <Flex direction="row-reverse">
+                <Flex.Item span={8}>span: 8-1</Flex.Item>
+                <Flex.Item span={8}>span: 8-2</Flex.Item>
+                <Flex.Item span={8}>span: 8-3</Flex.Item>
             </Flex>
         </div>
     </div>
